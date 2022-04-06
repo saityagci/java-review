9803c5d0a73a28969270ac3b3e6087026c56be6b
saityagci <saityagci@gmail.com>
2022-04-06 01:17:19 -0400

    Generics

--- generic/src/main/java/Team.java
+++ generic/src/main/java/Team.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
-public class Team {
+public class Team <T extends Player> {
     private String name;
-    private ArrayList<Player> members=new ArrayList<>();
+    private ArrayList<T> members=new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -10,5 +10,18 @@ public class Team {
 
     public String getName() {
         return name;
+    }
+    public boolean addPlayer(T player){
+        if (members.contains(player)){
+            System.out.println(player).getName()+" is already on this team");
+            return false;
+        }else{
+            members.add(player);
+            System.out.println(player).getName()+" picked for team "+this.name);
+            return true;
+        }
+    }
+    public int numPlayers(){
+        return this.members.size();
     }
 }
