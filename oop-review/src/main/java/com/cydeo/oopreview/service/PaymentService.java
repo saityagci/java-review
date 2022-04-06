6b333badfdb0eff3d75b20d32f85d8c26594b808
saityagci <saityagci@gmail.com>
2022-04-06 00:12:23 -0400

    Initial commit

---
+++ oop-review/src/main/java/com/cydeo/oopreview/service/PaymentService.java
@@ -0,0 +1,13 @@
+package com.cydeo.oopreview.service;
+
+
+import com.cydeo.oopreview.exception.InvalidPaymentStrategyException;
+import com.cydeo.oopreview.model.payment.PaymentResponse;
+import com.cydeo.oopreview.model.payment.AuthRequest;
+
+public interface PaymentService {
+    PaymentResponse auth(AuthRequest authRequest);
+
+    PaymentResponse auth3D(AuthRequest auth3DRequest);
+}
+//aa
