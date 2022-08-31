e9d347616796bdb341b37f91ce7907621c5f81ea
saityagci <saityagci@gmail.com>
2022-08-30 23:53:53 -0400

    generictest

--- generic/src/main/java/review/GenericTest.java
+++ generic/src/main/java/review/GenericTest.java
@@ -1,7 +1,21 @@
 package review;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericTest {
     public static void main(String[] args) {
+        List<Student> studentList= new ArrayList<>();
+        studentList.add(new Student("Max",101));
+        studentList.add(new Student("Adam",102));
+        studentList.add(new Student("Angela",103));
+        studentList.add(new Student("Malek",104));
+
+        List<Teacher> teacherList= new ArrayList<>();
+        studentList.add(new Student("Joe",201));
+        studentList.add(new Student("Alicia",201));
+        studentList.add(new Student("Mark",201));
+
 
     }
 }
